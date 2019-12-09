c747d4749b07b2948af83e65039672bcf135c807
Eric Fitz <57112072+ekfitz@users.noreply.github.com>
2019-12-09 10:20:38 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
 var illum = document.getElementById("illum");
 illum.src = "images/ill.PNG";
-var desc = document.getElementById("desc");
-desc.innerHTML = "Now we know you. Welcome. You will know us soon";
