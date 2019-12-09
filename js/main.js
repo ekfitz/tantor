998a8030faac929da7e087261ab09837862858d8
Eric Fitz <57112072+ekfitz@users.noreply.github.com>
2019-12-09 09:37:51 -0700

    Skeleton
    Image

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var illum = document.getElementById("illum");
+illum.src = "images/ill.PNG";
+var desc = document.getElementById("desc");
+desc.innerHTML = "Now we know you. Welcome. You will know us soon";
